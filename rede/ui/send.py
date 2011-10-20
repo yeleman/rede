06f96081dfe1c60f0a79775eda34e4496da86940
ylm <alou@ylm.(none)>
2011-10-20 13:01:41 +0000

    correction d sebd.py

--- rede/ui/send.py
+++ rede/ui/send.py
@@ -3,6 +3,7 @@
 # maintainer: Alou
 
 from PyQt4 import QtGui
+from PyQt4.QtCore import Qt
 
 from common import REDEWidget
 
@@ -14,33 +15,28 @@ class SendWidget(REDEWidget):
         super(SendWidget, self).__init__(parent=parent, *args, **kwargs)
 
         vbox = QtGui.QVBoxLayout()
-        hbox1 = QtGui.QHBoxLayout()
-        hbox2 = QtGui.QHBoxLayout()
-        hbox3 = QtGui.QHBoxLayout()
-        hbox4 = QtGui.QHBoxLayout()
+        formbox = QtGui.QFormLayout()
 
-        hbox1.addWidget(QtGui.QLabel(_(u"Choice of sending")))
+        self.label = QtGui.QLabel(_(u"Choice of sending"))
+        self.label.setFont(QtGui.QFont("Times New Roman", 24))
+        self.label.setAlignment(Qt.AlignCenter)
+        formbox.addWidget(self.label)
 
         self.combobox = QtGui.QComboBox()
         self.combobox.addItem(_(u'SMS'))
         self.combobox.addItem(_(u'Internet'))
-        hbox2.addWidget(QtGui.QLabel(_(u"Type of mail")))
-        hbox2.addWidget(self.combobox)
-
-        hbox3.addWidget(QtGui.QLabel(_(u"details")))
-        hbox3.addWidget(QtGui.QLineEdit(_(u"This mailing will cost 4 SMS")))
+        formbox.addRow(_(u"Type of mail"), self.combobox)
+        formbox.addRow(_(u"details"), QtGui.QLineEdit(_(u"This mailing will cost 4 SMS")))
 
         cancel_but = QtGui.QPushButton(_(u"Cancel"))
         send_but = QtGui.QPushButton(_(u"Send"))
-        hbox4.addWidget(send_but)
-        hbox4.addWidget(cancel_but)
+        formbox.addWidget(send_but)
+        formbox.addWidget(cancel_but)
+
         send_but.clicked.connect(self.send)
         cancel_but.clicked.connect(self.cancel)
 
-        vbox.addLayout(hbox1)
-        vbox.addLayout(hbox2)
-        vbox.addLayout(hbox3)
-        vbox.addLayout(hbox4)
+        vbox.addLayout(formbox)
         self.setLayout(vbox)
 
     def send(self):
