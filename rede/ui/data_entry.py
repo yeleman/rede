95f68934f0c5bfe1099010bfb0a494c63d417133
Fadiga <ibfadiga@gmail.com>
2011-11-01 13:58:47 +0000

    Amélioration

--- rede/ui/data_entry.py
+++ rede/ui/data_entry.py
@@ -4,7 +4,8 @@
 
 from PyQt4 import QtGui, QtCore
 
-from common import REDEWidget, PageTitle, PageIntro, DateEdit
+from common import REDEWidget, PageTitle, PageIntro, FormLabel,\
+                                    DateEdit, IntLineEdit, FloatLineEdit
 
 
 class DataEntryWidget(REDEWidget):
@@ -20,8 +21,15 @@ class DataEntryWidget(REDEWidget):
         vbox.addWidget(self.title)
         vbox.addWidget(self.intro)
 
+        self.floatlineedit = FloatLineEdit()
+        self.inttlineedit = IntLineEdit()
         self.date = DateEdit(QtCore.QDate.currentDate())
-        vbox.addWidget(self.date)
 
+        gridbox.addWidget(FormLabel(_(u"Nombre positif")), 0, 1)
+        gridbox.addWidget(self.inttlineedit, 0, 2)
+        gridbox.addWidget(FormLabel(_(u"Nombre positif avec virgule")), 1, 1)
+        gridbox.addWidget(self.floatlineedit, 1, 2)
+        gridbox.addWidget(FormLabel(_(u"Date")), 2, 1)
+        gridbox.addWidget(self.date, 2, 2)
         vbox.addLayout(gridbox)
         self.setLayout(vbox)
